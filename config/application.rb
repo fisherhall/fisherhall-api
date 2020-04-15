40518d4bb1451adb4da359b2b851eb621c2a5ab6
Amos Chan <ac@amoschan.com>
2020-04-15 15:01:52 -0400

    Add Heroku deployment support (#2)
    * Move sqlite3 to development & test envs
    
    * Allow any domain to access api

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module FisherhallApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "localhost:3000"
+        origins "*"
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
