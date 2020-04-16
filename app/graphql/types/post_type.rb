e9ef9a1f27b7b080d9755ab0cc7d85e3569aca29
Amos Chan <ac@amoschan.com>
2020-04-16 09:10:35 -0400

    Use ISO8601DateTime for graphql dates

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,7 +4,7 @@ module Types
     field :title, String, null: true
     field :group, Types::GroupType, null: false
     field :content, String, null: false
-    field :published_at, String, null: true
+    field :published_at, GraphQL::Types::ISO8601DateTime, null: true
     field :banner_url, String, null: true
     field :kind, String, null: false
     field :slug, String, null: false
